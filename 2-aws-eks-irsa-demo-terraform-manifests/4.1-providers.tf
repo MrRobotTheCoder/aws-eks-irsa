9553c04240e843470fdbe460be403ab5906a2f8e
Abinash Sethi <sethiabinash123@gmail.com>
2025-07-13 21:15:26 -0400

    irsa-demo

---
+++ 2-aws-eks-irsa-demo-terraform-manifests/4.1-providers.tf
@@ -0,0 +1,20 @@
+# Terraform AWS Provider Block
+provider "aws" {
+    region = "us-east-1"  
+}
+
+# Retriving EKS Cluster Name using Terraform Remote State Data Source from 1-aws-eks-cluster-basics/5.2-aws-eks-outputs.tf
+data "aws_eks_cluster" "cluster" {
+  name = data.terraform_remote_state.eks.output.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = data.terraform_remote_state.eks.output.cluster_id
+}
+
+# Terraform Kubernetes Provider Block
+provider "kubernetes" {
+    host = data.terraform_remote_state.eks.output.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.output.cluster_certificate_authority_data)
+    token = data.aws_eks_cluster_auth.cluster.token
+}
