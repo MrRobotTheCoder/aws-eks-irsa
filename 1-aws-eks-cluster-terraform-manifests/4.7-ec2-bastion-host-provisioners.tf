16a1a7cd1dce6d19227bf5aa095208ab93a3dc38
Abinash Sethi <sethiabinash123@gmail.com>
2025-07-13 20:16:47 -0400

    updated null resource

--- 1-aws-eks-cluster-terraform-manifests/4.7-ec2-bastion-host-provisioners.tf
+++ 1-aws-eks-cluster-terraform-manifests/4.7-ec2-bastion-host-provisioners.tf
@@ -13,18 +13,21 @@ resource "null_resource" "copy_ec2_keys" {
 ## File Provisioner: Copies the terraform-key.pem file to /tmp/terraform-key.pem
   provisioner "file" {
     source      = "private-key/aws-devops-key.pem"
-    destination = "/home/ec2-user/aws-devops-key.pem"
+    destination = "/tmp/aws-devops-key.pem"
   }
 ## Remote Exec Provisioner: Using remote-exec provisioner fix the private key permissions on Bastion Host
   provisioner "remote-exec" {
     inline = [
+      "sudo mv /tmp/aws-devops-key.pem /home/ec2-user/aws-devops-key.pem",
+      "sudo chown ec2-user:ec2-user /home/ec2-user/aws-devops-key.pem",
       "sudo chmod 400 /home/ec2-user/aws-devops-key.pem"
     ]
   }
 ## Local Exec Provisioner:  local-exec provisioner (Creation-Time Provisioner - Triggered during Create Resource)
   provisioner "local-exec" {
-    command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
-    working_dir = "local-exec-output-files/"
+    #command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
+    command = "mkdir -p local-exec-output-files && echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> local-exec-output-files/creation-time-vpc-id.txt"
+    #working_dir = "local-exec-output-files/"
     #on_failure = continue
   }
 
