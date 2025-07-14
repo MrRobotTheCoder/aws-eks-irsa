29c9ac657d65b7f3fdb6719fe3097126f3e925f7
Abinash Sethi <sethiabinash123@gmail.com>
2025-07-13 21:22:43 -0400

    corrcted output

--- 2-aws-eks-irsa-demo-terraform-manifests/4.1-providers.tf
+++ 2-aws-eks-irsa-demo-terraform-manifests/4.1-providers.tf
@@ -5,16 +5,16 @@ provider "aws" {
 
 # Retriving EKS Cluster Name using Terraform Remote State Data Source from 1-aws-eks-cluster-basics/5.2-aws-eks-outputs.tf
 data "aws_eks_cluster" "cluster" {
-  name = data.terraform_remote_state.eks.output.cluster_id
+  name = data.terraform_remote_state.eks.outputs.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = data.terraform_remote_state.eks.output.cluster_id
+  name = data.terraform_remote_state.eks.outputs.cluster_id
 }
 
 # Terraform Kubernetes Provider Block
 provider "kubernetes" {
     host = data.terraform_remote_state.eks.output.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.output.cluster_certificate_authority_data)
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
     token = data.aws_eks_cluster_auth.cluster.token
 }
